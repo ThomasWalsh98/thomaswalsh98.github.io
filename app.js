6becc1c0017162475c4750916e1a7e98c068859f
ThomasWalsh98 <72092621+ThomasWalsh98@users.noreply.github.com>
2021-09-19 21:32:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,3 +34,90 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  var tt = document.getElementsByClassName("imgCaption");
+
+  if (n > slides.length){
+      slideIndex = 1
+    }
+
+  if (n < 1) {
+      slideIndex = slides.length
+    }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = tt[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  tt[slideIndex-1].className += " active";
+  captionText.innerHTML = tt[slideIndex-1].alt;
+}
+
+
+
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo2");
+  var captionText = document.getElementById("caption2");
+  var tt = document.getElementsByClassName("imgCaption2");
+
+  if (n > slides.length){
+      slideIndex2 = 1
+    }
+
+  if (n < 1) {
+      slideIndex2 = slides.length
+    }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = tt[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex2-1].style.display = "block";
+  tt[slideIndex2-1].className += " active";
+  captionText.innerHTML = tt[slideIndex2-1].alt;
+}
+
+
